9d724e3adb21bf44c9e0a0550d0cca610d7db00c
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-08-29 12:49:50 +0200

    es fatto

---
+++ es13/exercise-3-tuple-types-start.ts
@@ -0,0 +1,30 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// Add a tuple type annotation for this tuple.
+
+const countryPopulation: [string, number] = ["China", 1_412_600_000];
+
+// Change the `CountryWithPopulation` type into a tuple type.
+// This should fix the type error on Line 14.
+
+type CountryWithCurrency = [string, string, string];
+
+const countryCurrency: CountryWithCurrency = ["Italy", "Euro", "EUR"];
+
+// Add a tuple type annotation that prevents this tuple from being modified.
+// Use the readonly modifier and the tuple type syntax: readonly [type, type]
+// This will cause a type error. Remove the code that now has a type error.
+
+type countryLanguage = readonly [string, string]
+
+const countryLanguage = ["Greece", "Greek"];
+
+countryLanguage.reverse();
+
+console.log(countryLanguage);
+
+// ----
+
+export {};
